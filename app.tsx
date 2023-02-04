5105f7510b80b3d3cb5e4d5abacca4f7bf2d501c
William Bradshaw <will.bradshaw50@gmail.com>
2023-02-04 12:46:59 +1100

    boilerplate DONE!

---
+++ app.tsx
@@ -0,0 +1,17 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { h } from "nano_jsx";
+
+export default function App() {
+    return(
+        <html>
+            <body>
+                <p>Hello World!</p>
+            </body>
+        </html>
+    )
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+import { parse } from "std";
+import { renderSSR } from "nano_jsx";
+import App from "./app.tsx";
+
 export function add(a: number, b: number): number {
   return a + b;
 }
@@ -6,3 +10,20 @@ export function add(a: number, b: number): number {
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 }
+
+function handler(_req: Request) {
+  const html = renderSSR(App);
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
+
+const ac = new AbortController();
+const args = parse(Deno.args);
+Deno.serve({ signal: ac.signal, port: args.port }, handler);
+
+globalThis.addEventListener("unload", () => {
+  ac.abort()
+});
