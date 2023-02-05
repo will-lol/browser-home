5e758b2a1335c3f6875a60373b3c33f2c35f69db
William Bradshaw <will.bradshaw50@gmail.com>
2023-02-05 22:27:12 +1100

    sigfig logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,58 @@
 import useAsset from "ultra/hooks/use-asset.js";
-// Twind
 import { tw } from "./twind/twind.ts";
+
+function sigFig(num: number, figs: number): string {
+  const numStringArray= num.toString().split(".");
+  const integers = Array.from(numStringArray[0]);
+  let decimals: Array<string>;
+  try {
+      decimals = Array.from(numStringArray[1]);
+  } catch {
+      decimals = [];
+  }
+  let disparity = figs - (integers.length + decimals.length) //negative if there figs to remove, positive if there are figs to add
+
+  if (disparity > 0) {
+      while (disparity != 0) {
+          decimals.push("0")
+          disparity--
+      }
+  } else {
+      while ((decimals.length > 0) && (disparity != 0)) {
+          decimals.pop();
+          disparity++;
+      }
+
+      if (disparity == 0) {
+          return repairString(integers, decimals);
+      } else {
+          let position = integers.length - 1;
+          while (disparity != 0) {
+              integers[position] = "0";
+              position--
+              disparity++
+          }
+      }
+  }
+
+  return(repairString(integers, decimals))
+
+  function repairString(integers: Array<string>, decimals: Array<string>): string {
+      if (decimals.length > 0) {
+          return integers.join("") + "." + decimals.join("");
+      } else {
+          return integers.join("")
+      }
+  }
+}
 
 export default function App() {
   const currentYear = new Date().getFullYear();
-  const yearOffset = Date.UTC(currentYear,0,1,0,0,0,0);
-  const endOfYear = Date.UTC(currentYear + 1,0,1,0,0,0,0);
+  const startOfYear = Date.UTC(currentYear,0,1,0,0,0,0);
+  const endOfYear = Date.UTC(currentYear + 1,0,0,0,0,0,0);
   const currentTime = Date.now();
-  const yearProgress = (currentTime - yearOffset) / (endOfYear - yearOffset);
-  const yearProgressPercentage = Math.trunc(yearProgress * 1000000)/10000
+  const yearProgress = (currentTime - startOfYear) / (endOfYear - startOfYear);
+  const yearProgressPercentage = sigFig(yearProgress * 100, 5);
 
   return (
     <html lang="en">
